d78edc2f95029adc27de2f5860f5dca2a85b8685
titulus <titulus.web@gmail.com>
2016-10-22 12:44:46 +0300

    another huge refactoring

--- js-interface.js
+++ js-interface.js
@@ -1,50 +1,79 @@
 'use strict';
 
-function jsInterface(method_variable) {
-  let methods;
-  switch(typeOfObject(method_variable)) {
-    case 'Array' : methods = method_variable; break;
-    case 'String' : methods = [method_variable]; break;
-    default: throw new Error('method_variable must be an Array or String');
-  }
-  function Interface(){
-    for (let i of methods) {
-      this[i] = function(){ throw new Error(`${i} is not implemented in interface`) };
-    }
-  };  
-  return Interface;
-};
-jsInterface.define = function(context,name,_interface) {
-  if (typeOfObject(_interface) !== 'Function') throw new Error('You should define Function');
-  const interface_example = new _interface();
-  for (let i in interface_example) {
-    interface_example[i] = interface_example[i].bind(context);
-  };
-  jsInterface_defineProperty(context,name,_interface,interface_example);
-};
-function jsInterface_defineProperty(context,name,_interface,implementation) {
-  Object.defineProperty(context,name,{
-    configurable: true,
-    get: function(){return implementation;},
-    set: function(value){
-      jsInterface_connect(this,name,_interface,value);
-    }
-  });
-};
-function jsInterface_connect(context,name,_interface,implementation) {
-  if (typeOfObject(implementation) !== 'Object') throw new Error('You should connect Object');
-  const interface_example = {};
-  for (let i in (new _interface)) {
-    interface_example[i] = function() {
-      if (!implementation.hasOwnProperty(i)) throw new Error(`Method ${i} is not found in implementation`);
-      implementation[i].apply(context,arguments);
+function jsInterface(input) {
+    let defaultImplementation;
+    let interfaceProperties;
+    switch(typeOfObject(input)) {
+        case 'String' :
+            interfaceProperties = [input];
+            defaultImplementation = createObjectOfErrors(interfaceProperties);
+            break;
+        case 'Array' :
+            interfaceProperties = input.slice();
+            defaultImplementation = createObjectOfErrors(interfaceProperties);
+            break;
+        case 'Function' :
+            defaultImplementation = new input();
+            interfaceProperties = Object_keysWithInherited(defaultImplementation);
+            break;
+        case 'Object' :
+            defaultImplementation = input;
+            interfaceProperties = Object_keysWithInherited(defaultImplementation);
+            break;
+        default: throw new Error('jsInterface accept only String, Array, Function or Object.');
     };
-  };
-  jsInterface_defineProperty(context,name,_interface,interface_example);
+    function createObjectOfErrors(methodList) {
+        const obj = {};
+        for (let i of methodList) {
+            obj[i] = function(){ throw new Error(`${i} is not implemented in interface`) };
+        };
+        return obj;
+    };
+    function Object_keysWithInherited(obj) {
+        let keys = [];
+        for (let i in obj) {
+            keys.push(i);
+        };
+        return keys;
+    };
+    
+    function jsInterface_define(context,name) {
+        jsInterface_connect(context,name,defaultImplementation);
+    };
+    function jsInterface_connect(context,name,implementation) {
+        if (typeOfObject(implementation) !== 'Object') throw new Error('You should connect Object');
+        const wrappedImplementation = wrapWithContext(context,implementation);
+        jsInterface_defineProperty(context,name,wrappedImplementation);
+    };
+    function wrapWithContext(context,implementation) {
+        const wrappedImplementation = {};
+        for (let i of interfaceProperties) {
+            wrappedImplementation[i] = function() {
+                if (typeof implementation[i] === 'undefined') throw new Error(`Method ${i} is not found in implementation`);
+                implementation[i].apply(context,...arguments);
+            };
+        };
+        return wrappedImplementation;
+    };
+    function jsInterface_defineProperty(context,name,implementation) {
+        Object.defineProperty(context,name,{
+            configurable: true,
+            get: function(){return implementation;},
+            set: function(value){
+                jsInterface_connect(this,name,value);
+            }
+        });
+    }
+
+    function Interface(){
+        this.define = jsInterface_define;
+    };
+    return new Interface;
 };
 
+
 function typeOfObject(obj) {
-  return Object.prototype.toString.call(obj).slice(8, -1)
+    return Object.prototype.toString.call(obj).slice(8, -1)
 };
 
 module.exports = jsInterface;
